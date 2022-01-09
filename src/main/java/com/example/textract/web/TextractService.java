4ac84c22877f0b60eb3e14041001969958aa17b0
wangobango <ramon.dyzman@gmail.com>
2022-01-09 23:40:58 +0100

    working stuff

--- src/main/java/com/example/textract/web/TextractService.java
+++ src/main/java/com/example/textract/web/TextractService.java
@@ -1,19 +1,24 @@
 package com.example.textract.web;
 
 import com.example.textract.core.AnalyseDocument;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.textract.model.Block;
 
 import javax.annotation.PreDestroy;
-import java.io.InputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.List;
 
 @Service
 public class TextractService {
 
-    private AnalyseDocument analyseDocument;
+    private final AnalyseDocument analyseDocument;
 
     @Autowired
     public TextractService(AnalyseDocument analyseDocument) {
@@ -26,8 +31,9 @@ public class TextractService {
         this.analyseDocument.close();
     }
 
-    public String extractInfoFromInvoice(InputStream inputStream) {
-        SdkBytes bytes = SdkBytes.fromInputStream(inputStream);
+    public String extractInfoFromInvoice(InputStream inputStream) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = extractImageFromDocument(inputStream);
+        SdkBytes bytes = SdkBytes.fromInputStream(byteArrayInputStream);
         List<Block> docInfo = analyseDocument.analyzeDoc(bytes);
 
         String result = "empty";
@@ -37,6 +43,20 @@ public class TextractService {
             result = block.blockType().toString();
         }
         return result;
+    }
+
+    private ByteArrayInputStream extractImageFromDocument(InputStream inputStream) throws IOException {
+        PDDocument document = PDDocument.load(inputStream);
+
+        PDFRenderer pdfRenderer = new PDFRenderer(document);
+        BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 1000, ImageType.RGB);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIOUtil.writeImage(bim, "png", outputStream);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+        document.close();
+        return byteArrayInputStream;
     }
 
 
